567f5ac2c0e01a50b8edd8e12d1c285bab88f20a
mvishiu11 <jakubmuszynski2103@gmail.com>
2025-08-01 17:32:41 +0200

    chore: add copyrights for FT0 files

--- Modules/FIT/FT0/src/AmplitudePostProcTask.cxx
+++ Modules/FIT/FT0/src/AmplitudePostProcTask.cxx
@@ -1,5 +1,18 @@
-// Copyright ...
-// \file   AmplitudePostProcTask.cxx
+// Copyright 2019-2020 CERN and copyright holders of ALICE O2.
+// See https://alice-o2.web.cern.ch/copyright for details of the copyright holders.
+// All rights not expressly granted are reserved.
+//
+// This software is distributed under the terms of the GNU General Public
+// License v3 (GPL Version 3), copied verbatim in the file "COPYING".
+//
+// In applying this license CERN does not waive the privileges and immunities
+// granted to it by virtue of its status as an Intergovernmental Organization
+// or submit itself to any jurisdiction.
+///
+/// \file   AmplitudePostProcTask.cxx
+/// \author Jakub Muszyński jakub.milosz.muszynski@cern.ch
+/// \brief  Post-processing task for FT0 amplitude analysis
+///
 
 // O2 QC / framework
 #include "FT0/AmplitudePostProcTask.h"
