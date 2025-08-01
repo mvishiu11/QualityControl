1c8360d634a6cda56f21b3b7872a571d2c99f0dd
mvishiu11 <jakubmuszynski2103@gmail.com>
2025-08-01 16:19:34 +0200

    feat: add per-graph csutomization options

--- Framework/include/QualityControl/TrendingTaskConfig.h
+++ Framework/include/QualityControl/TrendingTaskConfig.h
@@ -32,6 +32,18 @@ struct TrendingTaskConfig : PostProcessingConfig {
   TrendingTaskConfig(std::string name, const boost::property_tree::ptree& config);
   ~TrendingTaskConfig() = default;
 
+  // graph style configuration
+  struct GraphStyle {
+    int   lineColor   = -1;
+    int   lineStyle   = -1;
+    int   lineWidth   = -1;
+    int   markerColor = -1;
+    int   markerStyle = -1;
+    float markerSize  = -1.f;
+    int   fillColor   = -1;
+    int   fillStyle   = -1;
+  };
+
   // this corresponds to one TTree::Draw() call, i.e. one graph or histogram drawing
   struct Graph {
     std::string name;
@@ -40,6 +52,7 @@ struct TrendingTaskConfig : PostProcessingConfig {
     std::string selection;
     std::string option; // the list of possible options are documented in TGraphPainter and THistPainter
     std::string errors;
+    GraphStyle style;
   };
 
   // legend configuration
--- Framework/src/TrendingTaskConfig.cxx
+++ Framework/src/TrendingTaskConfig.cxx
@@ -36,14 +36,34 @@ TrendingTaskConfig::TrendingTaskConfig(std::string id, const boost::property_tre
       for (const auto& [_, graphConfig] : graphsConfig.value()) {
         // first we use name of the graph, if absent, we use graph title, if absent, we use plot (object) name.
         const auto& name = graphConfig.get<std::string>("name", graphConfig.get<std::string>("title", plotConfig.get<std::string>("name")));
+        GraphStyle style;
+        style.lineColor    = graphConfig.get<int>("style.lineColor",   -1);
+        style.lineStyle    = graphConfig.get<int>("style.lineStyle",   -1);
+        style.lineWidth    = graphConfig.get<int>("style.lineWidth",   -1);
+        style.markerColor  = graphConfig.get<int>("style.markerColor", -1);
+        style.markerStyle  = graphConfig.get<int>("style.markerStyle", -1);
+        style.markerSize   = graphConfig.get<float>("style.markerSize", -1.f);
+        style.fillColor    = graphConfig.get<int>("style.fillColor",   -1);
+        style.fillStyle    = graphConfig.get<int>("style.fillStyle",   -1);
+
         graphs.push_back({ name,
                            graphConfig.get<std::string>("title", ""),
                            graphConfig.get<std::string>("varexp"),
                            graphConfig.get<std::string>("selection", ""),
                            graphConfig.get<std::string>("option", ""),
-                           graphConfig.get<std::string>("graphErrors", "") });
+                           graphConfig.get<std::string>("graphErrors", ""),
+                           style });
       }
     } else {
+      GraphStyle style;
+      style.lineColor    = plotConfig.get<int>("style.lineColor",   -1);
+      style.lineStyle    = plotConfig.get<int>("style.lineStyle",   -1);
+      style.lineWidth    = plotConfig.get<int>("style.lineWidth",   -1);
+      style.markerColor  = plotConfig.get<int>("style.markerColor", -1);
+      style.markerStyle  = plotConfig.get<int>("style.markerStyle", -1);
+      style.markerSize   = plotConfig.get<float>("style.markerSize", -1.f);
+      style.fillColor    = plotConfig.get<int>("style.fillColor",   -1);
+      style.fillStyle    = plotConfig.get<int>("style.fillStyle",   -1);
       graphs.push_back({ plotConfig.get<std::string>("name", ""),
                          plotConfig.get<std::string>("title", ""),
                          plotConfig.get<std::string>("varexp"),
