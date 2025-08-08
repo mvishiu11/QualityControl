1f40294cc0c6711803f6d7e16b690917d2bf1d2c
mvishiu11 <jakubmuszynski2103@gmail.com>
2025-08-08 14:09:13 +0200

    chore: link the laser aging post proc

--- Modules/FIT/FT0/include/FT0/LinkDef.h
+++ Modules/FIT/FT0/include/FT0/LinkDef.h
@@ -8,6 +8,7 @@
 #pragma link C++ class o2::quality_control_modules::ft0::MergedTreeCheck + ;
 #pragma link C++ class o2::quality_control_modules::ft0::PostProcTask + ;
 #pragma link C++ class o2::quality_control_modules::ft0::AmplitudePostProcTask + ;
+#pragma link C++ class o2::quality_control_modules::ft0::AgingLaserPostProcTask + ;
 #pragma link C++ class o2::quality_control_modules::ft0::GenericCheck + ;
 #pragma link C++ class o2::quality_control_modules::ft0::CFDEffCheck + ;
 #pragma link C++ class o2::quality_control_modules::ft0::FractionCheck + ;
